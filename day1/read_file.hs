cfa3cad99881f03b842e7a52c8efc52348a72c51
samfisch14 <97541667+samfisch14@users.noreply.github.com>
2024-12-01 15:37:53 -0600

    added comments to read_file.hs

--- day1/read_file.hs
+++ day1/read_file.hs
@@ -1,29 +1,43 @@
 import System.IO
 import Data.List
 
+-- convert string to integer
 readInt :: String -> Int
 readInt a = read a
 
+-- extract the first element of each sublist and convert to integer
 buildFirstList :: [[String]] -> [Int]
 buildFirstList li = map readInt $ map head li
 
+-- extract the last element of each sublist and convert to integer
 buildSecondList:: [[String]] -> [Int]
 buildSecondList li = map readInt $ map last li
 
+-- split multi-line string into a nested list of words
+-- each line is split into a list of words and all lines are combined into list of lists
 readNumLine :: String -> [[String]]
 readNumLine str = map words $ lines str
 
+-- absolute difference (distance) is calculated between two integers
 dist :: (Int, Int) -> Int
 dist a = abs $ (fst a) - (snd a)
 
+-- calculates total distance between two lists of integers
+-- each integer pair is passed to 'dist' and their results summed
 getDistance :: [Int] -> [Int] -> Int
 getDistance x y = foldl (+) 0 $ map dist $ zip x y
-	
+
 main :: IO ()
 main = do
+	-- read input file
 	contents <- readFile "d1_input.txt"
-	let n = readNumLine contents
+	-- turns file into nested list of words
+ 	let n = readNumLine contents
+  	-- builds and sorts list of integers from first element of each sublist
 	let a = sort $ buildFirstList n
+ 	-- builds and sorts list of integers from last element of each sublist
 	let b = sort $ buildSecondList n
+ 	-- calculates the distance between two sorted lists
 	let res = getDistance a b
+ 	-- final answer
 	print res 
